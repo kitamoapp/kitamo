5219f4e18dc6c11bf98b81f89be1da221ec4feaa
KitaMo PH <kitamoapp@gmail.com>
2025-08-31 12:47:42 +0000

    "OR CONTINUE WITH" should be in lower case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,9 +88,9 @@ export default function LoginPage() {
                 <div className="absolute inset-0 flex items-center">
                     <span className="w-full border-t" />
                 </div>
-                <div className="relative flex justify-center text-xs uppercase">
+                <div className="relative flex justify-center text-xs">
                     <span className="bg-card px-2 text-muted-foreground">
-                    Or continue with
+                    or continue with
                     </span>
                 </div>
                 </div>
