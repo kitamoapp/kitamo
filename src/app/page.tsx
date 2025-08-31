32cdb31b0bb1adf963ec5eab70137e2fa0701bd7
KitaMo PH <kitamoapp@gmail.com>
2025-08-31 12:46:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,36 +16,23 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 const GmailIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 48 48"
       {...props}
     >
-      <path d="M22 5.88V18a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-.56.31-1.08.8-1.39l8-4.5a2 2 0 0 1 2.4 0l8 4.5c.49.31.8.83.8 1.39z" fillOpacity=".05" />
-      <path d="M22 6.5l-10 6-10-6V18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6.5z" fill="#ea4335" />
-      <path d="M22 5.88l-10 6-10-6L12 2l10 3.88z" fill="#c5221f" />
-      <path d="M2 6l10 6 10-6-10-4L2 6z" fill="#4285f4" />
-      <path d="M2 18V6.5l10 6L2 18z" fillOpacity=".1" />
-      <path d="M22 18V6.5l-10 6L22 18z" fillOpacity=".1" />
+        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path><path fill="none" d="M0 0h48v48H0z"></path>
     </svg>
   );
 
 const FacebookIcon = (props: React.SVGProps<SVGSVGElement>) => (
-<svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
->
-    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" fill="#1877f2" stroke="#1877f2" />
-</svg>
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 48 48"
+      {...props}
+    >
+        <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path><path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-3.28c0-1.448,0.396-2.433,2.476-2.433l2.643,0v-4.48c-0.457-0.06-2.025-0.199-3.848-0.199c-3.808,0-6.417,2.336-6.417,6.583v3.714h-4.348v4.995h4.348v12.016h5.318V29.036z"></path>
+    </svg>
 );
 
 
