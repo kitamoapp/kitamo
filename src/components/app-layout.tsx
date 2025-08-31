8209b1ca2f37166bd3549be02feed045b33a5e63
KitaMo PH <kitamoapp@gmail.com>
2025-08-31 03:30:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,17 +1,14 @@
 
 import type { ReactNode } from 'react';
 import { Header } from '@/components/header';
-import { SubscriptionProvider } from '@/context/subscription-context';
 
 export function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <SubscriptionProvider>
-        <Header />
-        <main className="flex-1">
-          <div className="container py-8">{children}</div>
-        </main>
-      </SubscriptionProvider>
+      <Header />
+      <main className="flex-1">
+        <div className="container py-8">{children}</div>
+      </main>
     </div>
   );
 }
