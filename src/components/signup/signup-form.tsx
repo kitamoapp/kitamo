2bbc1f6259687ad2a967423155665938df4eea15
KitaMo PH <kitamoapp@gmail.com>
2025-09-01 05:09:01 +0000

    Given the updates, are we still going to encounter the error below? Is t

--- src/components/signup/signup-form.tsx
+++ src/components/signup/signup-form.tsx
@@ -1,8 +1,8 @@
 
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Card,
