009815b3ba6a265dfa006a51cb3820fb701df96a
KitaMo PH <kitamoapp@gmail.com>
2025-09-09 00:36:47 +0000

    Try fixing this error: `Build Error: Module parse failed: Unexpected tok

--- src/next.config.ts
+++ src/next.config.ts
@@ -25,6 +25,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (
+    config,
+    { isServer }
+  ) => {
+    // This is to prevent the error: "Module not found: Can't resolve 'react-native'"
+    // See: https://github.com/facebook/react-native/issues/28773
+    if (!isServer) {
+        config.resolve.alias['react-native$'] = 'react-native-web';
+    }
+
+    return config
+  },
 };
 
 if (process.env.NODE_ENV === 'development') {
